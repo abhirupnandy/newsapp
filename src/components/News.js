45b197a3cc47a0f6d8e32410bb51883afb55cd1c
Abhirup Nandy <abhirupnandy.online@gmail.com>
2022-04-14 23:39:23 +0530

    Pagination buttons

--- src/components/News.js
+++ src/components/News.js
@@ -8,29 +8,63 @@ export class News extends Component {
 		console.log('News constructor');
 		this.state = {
 			articles: this.articles,
+			page: 1,
 			loading: false,
 		};
 	}
 
 	async componentDidMount() {
 		console.log('News componentDidMount');
-		let url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=d870bf50d7634ab292e65343fd3bfe4b';
+		let url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=d870bf50d7634ab292e65343fd3bfe4b&page=1&pageSize=20';
 		let data = await fetch(url);
 		let parsedData = await data.json();
 		console.log(parsedData);
-		this.setState({ articles: parsedData.articles });
+		this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults });
 	}
+
+	handlePrevClick = async () => {
+		let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=d870bf50d7634ab292e65343fd3bfe4b&page=${this.state.page - 1}&pageSize=20`;
+		let data = await fetch(url);
+		let parsedData = await data.json();
+		console.log(parsedData);
+		this.setState({
+			articles: parsedData.articles,
+			page: this.state.page - 1,
+		});
+	};
+
+	handleNextClick = async () => {
+		if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+		} else {
+			let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=d870bf50d7634ab292e65343fd3bfe4b&page=${this.state.page + 1}&pageSize=20`;
+			let data = await fetch(url);
+			let parsedData = await data.json();
+			console.log(parsedData);
+			this.setState({
+				articles: parsedData.articles,
+				page: this.state.page + 1,
+			});
+		}
+	};
+
 	render() {
 		return (
 			<div className='container my-3'>
-				<h2>News-Bytes Daily Top headlines</h2>
-				{/* {this.state.articles.map((element) => {
-					console.log(element);
-				})} */}
+				<h1 className='text-center' style={{ fontSize: '420%' }}>
+					News-Bytes Daily Top headlines
+				</h1>
+				<div className='container d-flex justify-content-between'>
+					<button type='button' className='btn btn-info btn-lg' onClick={this.handlePrevClick}>
+						&larr; Previous
+					</button>
+					<button type='button' className='btn btn-info btn-lg' onClick={this.handleNextClick}>
+						Next &rarr;
+					</button>
+				</div>
 				<div className='row'>
 					{this.state.articles.map((element) => {
 						return (
-							<div className='col-md-4' key={element.url}>
+							<div className='col-md-3' key={element.url}>
 								<NewsItem
 									title={element.title ? element.title.slice(0, 50) : ''}
 									description={element.description ? element.description.slice(0, 80) : ''}
@@ -40,6 +74,14 @@ export class News extends Component {
 							</div>
 						);
 					})}
+				</div>
+				<div className='container d-flex justify-content-between'>
+					<button type='button' className='btn btn-dark btn-lg' onClick={this.handlePrevClick}>
+						&larr; Previous
+					</button>
+					<button type='button' className='btn btn-dark btn-lg' onClick={this.handleNextClick}>
+						Next &rarr;
+					</button>
 				</div>
 			</div>
 		);
